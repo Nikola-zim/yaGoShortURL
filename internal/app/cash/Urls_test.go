d8c3687a9162fd6056c0552058eb10230578ee5e
NikolaZim <n.zimenkov.2020@gmail.com>
2023-03-12 00:39:43 +0300

    gitTest5

---
+++ internal/app/cash/Urls_test.go
@@ -0,0 +1,113 @@
+package cash
+
+import (
+	"reflect"
+	"sync"
+	"testing"
+)
+
+func TestNewCash(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Cash
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewCash(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewCash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewUrls(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Urls
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewUrls(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewUrls() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUrls_ReadURLFromCash(t *testing.T) {
+	type fields struct {
+		urlsMap map[string]string
+		mux     sync.RWMutex
+		wg      sync.WaitGroup
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &Urls{
+				urlsMap: tt.fields.urlsMap,
+				mux:     tt.fields.mux,
+				wg:      tt.fields.wg,
+			}
+			got, err := u.ReadURLFromCash(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadURLFromCash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ReadURLFromCash() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUrls_WriteURLInCash(t *testing.T) {
+	type fields struct {
+		urlsMap map[string]string
+		mux     sync.RWMutex
+		wg      sync.WaitGroup
+	}
+	type args struct {
+		string2 string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &Urls{
+				urlsMap: tt.fields.urlsMap,
+				mux:     tt.fields.mux,
+				wg:      tt.fields.wg,
+			}
+			got, err := u.WriteURLInCash(tt.args.string2)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WriteURLInCash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("WriteURLInCash() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
