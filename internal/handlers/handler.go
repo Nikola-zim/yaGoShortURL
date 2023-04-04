851f326514f5977651e6d9a5e1a787614cd0e710
NikolaZim <n.zimenkov.2020@gmail.com>
2023-04-04 23:39:56 +0300

    tryZip3

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"compress/flate"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	"io"
 	"log"
 	"yaGoShortURL/internal/static"
 )
@@ -33,17 +33,17 @@ func NewHandler(service addAndGetURLService, cfg static.ConfigInit) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 	router.Use(func(c *gin.Context) {
-		if c.Request.Header.Get("Content-Encoding") == "deflate" {
+		if c.Request.Header.Get("Content-Encoding") == "gzip" {
 			// Читаем тело запроса в сжатом формате
 			reader := flate.NewReader(c.Request.Body)
 			defer reader.Close()
 
 			// Заменяем тело запроса на распакованные данные
-			data, err := ioutil.ReadAll(reader)
+			data, err := io.ReadAll(reader)
 			if err != nil {
 				log.Fatal(err)
 			}
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(data))
 
 			// Устанавливаем заголовок Content-Encoding в значение "identity"
 			c.Request.Header.Set("Content-Encoding", "identity")
